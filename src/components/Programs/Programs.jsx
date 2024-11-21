dcef86c44c73af79f37e37d81f7e30b1602baa61
ZoyaaaZoy <rifkyandriansyah444@gmail.com>
2024-11-22 00:10:05 +0700

    update 22/11/2024

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -17,18 +17,30 @@ const Programs = () => {
             <div className="wrapper-icon">
               <FaPodcast className='icon-programs' />
               <h1 className="title-icon">Podcast</h1>
+              <p className="subtitle-icon">
+                Sebuah program diskusi seru yang membahas kisah horor, mitos, dan kejadian aneh dari berbagai belahan dunia.
+              </p>
             </div>
             <div className="wrapper-icon">
               <BiMoviePlay className='icon-programs' />
               <h1 className="title-icon">Short Movie</h1>
+              <p className="subtitle-icon">
+                Koleksi film pendek bertema horor yang menampilkan kisah-kisah menegangkan, penuh kejutan, dan kadang menyeramkan
+              </p>
             </div>
             <div className="wrapper-icon">
               <MdOutlineExplore className='icon-programs' />
               <h1 className="title-icon">Explore</h1>
+              <p className="subtitle-icon">
+                Program ini menyuguhkan kisah-kisah menyeramkan dengan gaya narasi yang dramatis dan penuh emosi.
+              </p>
             </div>
             <div className="wrapper-icon">
               <RiSpeakLine className='icon-programs' />
               <h1 className="title-icon">Story Telling</h1>
+              <p className="subtitle-icon">
+                Petualangan langsung ke lokasi-lokasi misterius, seperti tempat angker, bangunan tua, atau situs penuh sejarah.
+              </p>
             </div>
           </div>
           
